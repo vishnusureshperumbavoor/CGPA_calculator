893d40450492468a3d59ce36c7b66af99b0b7e9d
vishnusureshperumbavoor <vishnusureshperumbavoor@gmail.com>
2023-08-01 20:05:35 +0530

    arranged order and removed semester cgpa calculation

--- ct_2021.py
+++ ct_2021.py
@@ -12,75 +12,74 @@ st.title("CT Revision 2021")
 
 last_4_sem = {
     "Semester 3": {
-        "1. Computer Organisation": 4,
-        "2. Programming in C": 3,
-        "3. Database Management Systems": 3,
-        "4. Digital Computer Fundamentals": 3,
-        "5. Programming in C Lab": 1.5,
-        "6. Database Management System lab":1.5,
-        "7. Digital Computer Fundamentals Lab":1.5,
-        "8. Web Technology lab":2.5,
-        "9. Computer System Hardware Lab":0,
+        "3139. Computer System Hardware Lab":0,
+        "3138. Web Technology lab":2.5,
+        "3137. Digital Computer Fundamentals Lab":1.5,
+        "3136. Database Management System lab":1.5,
+        "3135. Programming in C Lab": 1.5,
+        "3134. Digital Computer Fundamentals": 3,
+        "3133. Database Management Systems": 3,
+        "3132. Programming in C": 3,
+        "3131. Computer Organisation": 4,
     },
     "Semester 4": {
-        "1. Object Oriented Programming": 4,
-        "2. Computer Communication and Networks": 3,
-        "3. Data Structures": 4,
-        "4. Community Skills in Indian knowledge system": 0,
-        "5. Object Oriented Programming Lab": 1.5,
-        "6. Web Programming Lab":0,
-        "7. Data Structures Lab":1.5,
-        "8. Application Development Lab":0,
-        "8. Minor Project":2,
-        "9. Internship II":3,
+        "5009. Internship II":3,
+        "4139. Application Development Lab":0,
+        "4138. Data Structures Lab":1.5,
+        "4137. Web Programming Lab":0,
+        "4136. Object Oriented Programming Lab": 1.5,
+        "4133. Data Structures": 4,
+        "4132. Computer Communication and Networks": 3,
+        "4131. Object Oriented Programming": 4,
+        "4006. Minor Project":2,
+        "4001. Community Skills in Indian knowledge system": 0,
     },
     "Semester 5": {
-        "1. Project Management and Software Engineering": 0,
-        "2. Embedded System and Real time Operating System": 4,
-        "3. Program elective course (Virtualisation Technology and Cloud Computing / Ethical Hacking / Fundamentals of Artificial Intelligence and Machine Learning)": 4,
-        "4. Community Skills in Indian knowledge system": 4,
-        "5. Embedded Systems and Real Time Operating System Lab": 1.5,
-        "6. System Administration Lab":1.5,
-        "7. Program elective course lab (Virtualisation Technology and cloud computing Lab / Ethical Hacking Lab / Fundamentals of Artificial Intelligence and Machine Learning Lab)":1.5,
-        "8. Seminar":1,
-        "9. Major Project":0,
+        "6009. Major Project":0,
+        "5139. Virtualisation Technology and cloud computing Lab / Ethical Hacking Lab / Fundamentals of Artificial Intelligence and Machine Learning Lab":1.5,
+        "5138. System Administration Lab":1.5,
+        "5137. Embedded Systems and Real Time Operating System Lab": 1.5,
+        "5133. Community Skills in Indian knowledge system": 4,
+        "5132. Virtualisation Technology and Cloud Computing / Ethical Hacking / Fundamentals of Artificial Intelligence and Machine Learning": 4,
+        "5131. Embedded System and Real time Operating System": 4,
+        "5008. Seminar":1,
+        "5002. Project Management and Software Engineering": 0,
     },
     "Semester 6": {
-        "1. Entrepreneurship and Startup": 4,
-        "2. Embedded System and Real time Operating System": 4,
-        "3. Program elective course (Software Testing / Internet of Things / Server Administration)": 4,
-        "4. Open elective course (Introduction to IoT / Fundamentals of Web Technology / Multimedia / Cloud Computing)": 0,
-        "5. Indian Constitution": 2.5,
-        "6. Computer Network Engineering Lab": 1.5,
-        "7. Smart Device Programming Lab":1.5,
-        "8. Program elective course lab (Software Testing Lab / Internet of Things Lab / Server Administration Lab)":1.5,
-        "9. Major Project":4,
+        "6139. Program elective course lab (Software Testing Lab / Internet of Things Lab / Server Administration Lab)":1.5,
+        "6138. Smart Device Programming Lab":1.5,
+        "6137. Computer Network Engineering Lab": 1.5,
+        "6132. Open elective course (Introduction to IoT / Fundamentals of Web Technology / Multimedia / Cloud Computing)": 0,
+        "6131. Program elective course (Software Testing / Internet of Things / Server Administration)": 4,
+        "6009. Major Project":4,
+        "6002. Indian Constitution": 2.5,
+        "6001. Entrepreneurship and Startup": 4,
     },
 }
 
 semesters_regular = {
     "Semester 1": {
-        "1. Communication Skills in English": 4,
-        "2. Mathematics I": 5,
-        "3. Applied Physics I": 3,
-        "4. Applied Chemistry": 3,
-        "5. Engineering Graphics": 1.5,
-        "6. Applied Chemistry Lab": 1,
-        "7. Introduction to IT systems Lab": 2,
-        "8. Sports and Yoga": 1,
+        "1009. Sports and Yoga": 1,
+        "1008. Introduction to IT systems Lab": 2,
+        "1007. Chemistry Lab": 1,
+        "1005. Engineering Graphics": 1.5,
+        "1004. Chemistry": 3,
+        "1003. Physics I": 3,
+        "1002. Mathematics I": 5,
+        "1001. English": 4,
     },
     "Semester 2": {
-        "1. Mathematics II": 4,
-        "2. Applied Physics II": 3,
-        "3. Environmental Science":0,
-        "4. Fundamentals of Electrical & Electronics Engineering": 3,
-        "5. Problem Solving and Programming": 3,
-        "6. Communication Skills in English Lab": 1.5,
-        "7. Applied Physics Lab": 1,
-        "8. Fundamentals of Eletrical & Electronics Engineering Lab":0,
-        "9. Problem Solving and Programming Lab":0,
-        "10. Engineering Workshop Practice": 1.5,
-        "11. Internship I": 2,
+        "3009. Internship I": 2,
+        "2139. Problem Solving and Programming Lab":0,
+        "2131. Problem Solving and Programming": 3,
+        "2039. Fundamentals of Eletrical & Electronics Engineering Lab":0,
+        "2031. Fundamentals of Electrical & Electronics Engineering": 3,
+        "2009. Engineering Workshop Practice": 1.5,
+        "2008. Communication Skills in English Lab": 1.5,
+        "2006. Physics Lab": 1,
+        "2003. Physics II": 3,
+        "2002. Mathematics II": 4,
+        "2001. Environmental Science":0,
     },
     "Semester 3": last_4_sem["Semester 3"],
     "Semester 4": last_4_sem["Semester 4"],
@@ -130,7 +129,7 @@ def get_credit_points(grade):
 
 
 for semester, subjects in semesters.items():
-    st.subheader(semester)
+    st.header(semester)
     semester_grades = []
     for i, subject_data in enumerate(subjects.items(), start=1):
         subject, credit_hours = subject_data[0], subject_data[1]
@@ -139,56 +138,52 @@ for semester, subjects in semesters.items():
         grades.append(st.selectbox(f"{subject} (Credits: {credit_hours})", ("S", "A", "B", "C", "D", "E", "F"), key=key))
         semester_grades.append(grades[-1])
 
-    col1, col2 = st.columns(2)
+    # col1, col2 = st.columns(2)
 
-    with col2:
-        semester_cgpa_button_key = f"semester_cgpa_button_{semester}"
-        semester_cgpa_button = st.button("Calculate Semester CGPA", key=semester_cgpa_button_key)
+    # with col2:
+    #     semester_cgpa_button_key = f"semester_cgpa_button_{semester}"
+    #     semester_cgpa_button = st.button("Calculate Semester CGPA", key=semester_cgpa_button_key)
 
-    with col1:
-        cumulative_cgpa_button_key = f"cumulative_cgpa_button_{semester}"
-        cumulative_cgpa_button = st.button("Calculate CGPA till Semester", key=cumulative_cgpa_button_key)
+    # with col1:
+    # cumulative_cgpa_button_key = f"cumulative_cgpa_button_{semester}"
+    # cumulative_cgpa_button = st.button("Calculate CGPA till Semester", key=cumulative_cgpa_button_key)
 
-    if semester_cgpa_button:
-        semester_credit_points = 0
-        semester_credits = sum(subject_data[1] for subject_data in subjects.items())
+    # if semester_cgpa_button:
+    #     semester_credit_points = 0
+    #     semester_credits = sum(subject_data[1] for subject_data in subjects.items())
 
-        for grade, subject_data in zip(semester_grades, subjects.items()):
-            credit_hours = subject_data[1]
-            semester_credit_points += get_credit_points(grade) * credit_hours
+    #     for grade, subject_data in zip(semester_grades, subjects.items()):
+    #         credit_hours = subject_data[1]
+    #         semester_credit_points += get_credit_points(grade) * credit_hours
 
-        semester_cgpa = semester_credit_points / semester_credits
-        st.write(f"{semester} CGPA: {semester_cgpa:.2f}")
+    #     semester_cgpa = semester_credit_points / semester_credits
+    #     st.write(f"{semester} CGPA: {semester_cgpa:.2f}")
 
-    if cumulative_cgpa_button:
-        cumulative_credit_points = 0
-        cumulative_credits = 0
-        selected_semester = semester
+    # if cumulative_cgpa_button:
+    cumulative_credit_points = 0
+    cumulative_credits = 0
+    selected_semester = semester
 
-        for sem, subjects in semesters.items():
-            for subject, credit_hours in subjects.items():
-                if sem <= selected_semester:
-                    cumulative_credits += credit_hours
-                    index = course_names.index(subject)
-                    grade = grades[index]
-                    cumulative_credit_points += get_credit_points(grade) * credit_hours
+    for sem, subjects in semesters.items():
+        for subject, credit_hours in subjects.items():
+            if sem <= selected_semester:
+                cumulative_credits += credit_hours
+                index = course_names.index(subject)
+                grade = grades[index]
+                cumulative_credit_points += get_credit_points(grade) * credit_hours
 
-        cumulative_cgpa = cumulative_credit_points / cumulative_credits
-        st.write(f"CGPA till {selected_semester}: {cumulative_cgpa:.2f}")
-
-
-
-
+    cumulative_cgpa = cumulative_credit_points / cumulative_credits
+    st.subheader(f"CGPA till {selected_semester}: {cumulative_cgpa:.2f}")
 
 # Display the CGPAs to the user
-st.subheader("CGPA")
-if semester_cgpas:
-    st.write("Semester CGPAs:")
-    for semester, cgpa in zip(semesters.keys(), semester_cgpas):
-        st.write(f"{semester}: {cgpa}")
+# st.subheader("CGPA")
+# if semester_cgpas:
+#     st.write("Semester CGPAs:")
+#     for semester, cgpa in zip(semesters.keys(), semester_cgpas):
+#         st.write(f"{semester}: {cgpa}")
     
-    st.write("Cumulative CGPAs:")
-    for semester, cgpa in zip(semesters.keys(), cumulative_cgpas):
-        st.write(f"{semester}: {cgpa}")
-else:
-    st.write("No grades selected yet.")
+#     st.write("Cumulative CGPAs:")
+#     for semester, cgpa in zip(semesters.keys(), cumulative_cgpas):
+#         st.write(f"{semester}: {cgpa}")
+# else:
+#     st.write("No grades selected yet.")
